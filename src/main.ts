8aff858e110fc3961a2a333885d489170d7e62f5
CREAACT <dzhabborovruslan8@gmail.com>
2024-07-28 00:31:52 +0300

    edit error

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001','https://netcom.onrender.com/'],
   });
 
   const config = new DocumentBuilder()
