8378d74114261f4918df335ecad71768ae593e6f
CREAACT <dzhabborovruslan8@gmail.com>
2024-07-29 00:50:50 +0300

    изменил адрес фронта

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://client-foax.vercel.app'],
+    origin: ['http://localhost:3001', 'https://netcom-one.vercel.app'],
   });
 
   const config = new DocumentBuilder()
